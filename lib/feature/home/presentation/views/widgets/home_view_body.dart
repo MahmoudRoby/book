86104212e9cb33df0a324ea80c1d9f3970da2632
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2024-12-16 18:50:21 +0200

    add text style and text 'best seller'

--- lib/feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/style.dart';
 import 'package:bookly/feature/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/feature/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -9,9 +10,16 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SafeArea(
       child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
         children: [
+        
           CustomAppBar(),
-          FeaturedListView()
+          FeaturedListView(),
+          SizedBox(height: 40,),
+          Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: Text('Best Seller',style: Stylies.titleMedium,),
+          ),
         ],
       ),
     );

59e08c15459a6d30665cb97d172510b59745dd1c
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2024-12-19 01:50:18 +0200

    cubit stata and function for newset books

--- lib/feature/home/presentation/manger/featured books cubit/featured_cubit_cubit.dart
+++ lib/feature/home/presentation/manger/featured books cubit/featured_cubit_cubit.dart
@@ -9,10 +9,11 @@ class FeaturedCubitCubit extends Cubit<FeaturedCubitState> {
   FeaturedCubitCubit(this.homeRepo) : super(FeaturedCubitInitial());
 
   final HomeRepo homeRepo;
-
+//عملت اوبجيكت من الريبو عشان استخدم الدوال اللى فيه
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedCubitLoading());
     var result = await homeRepo.fetchFeatureBooks();
+    //هنا الدلة دى عشان انا مستخدم Either بحدد من خلال اليمين و الشمال اذا كانت ايرور او نجحت
     result.fold((fauiler) {
       emit(FeaturedCubitFailure(errorMessage: fauiler.errMessage));
     }, (books) {
---
+++ lib/feature/home/presentation/manger/newset books cubit/newset_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/feature/home/data/model/book_model/book_model.dart';
+import 'package:bookly/feature/home/data/repo/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> featchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailuer(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
+}

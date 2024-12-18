9b44736d74bfab379a5117f7e83f872167aa6f33
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2024-12-18 19:27:34 +0200

    create api service

---
+++ lib/feature/home/data/repo/home_repo.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/feature/home/data/model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  /*
+  /تحديد ايه اللى انا عمله فى الشاشة بالظبط
+  اللى يهمنى ان الدوال تتنفذ مش مهم ازاى
+  انا هحدد الاسم و الداتا اللى داخلة و اللى راجعه
+  */
+  Future<Either<Failure,List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+}
+// تحديد راجع الدوال اذا كان فاشل او ناجح(bookModel)

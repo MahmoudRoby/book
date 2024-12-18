715d0666e9da23ed551ddac9af071d3f1793a582
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2024-12-18 21:45:48 +0200

    add failure class and repo implementation function

--- lib/feature/home/data/repo/home_repo_impl.dart
+++ lib/feature/home/data/repo/home_repo_impl.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:bookly/core/error/failure.dart';
 import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/feature/home/data/model/book_model/book_model.dart';
 import 'package:bookly/feature/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -14,21 +17,43 @@ class HomeRepoImpl implements HomeRepo {
       var data = await apiService.get(
           endPoint:
               'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
-              
+
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
     } on Exception catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
     }
-    // throw UnimplementedError();
+    return left(ServerFailure(e.toString()));
   }
-
+/*
+انا بعمل اوبجيكت من apiService وطبعا لازم اضيفه فى الكونستركتور
+استقبله فى متغير اسمه data كل ده عشان استدعى دالة get من api class
+هيطلب منى الاند بوينت هديهاله وده هيرجعلى ماب هعمل عليها فور لوب 
+الراجع بتاعها هستقبله فى ليست عن طريق .add
+هعمل تراى و كاتش على المتغير كله
+لو كان الدنيا تمام هرجع الناحية اليمين من Either
+لو عمل كاتش يبقى اسأله انت جاى من dioExption ولا لأ
+*/
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+    }
+    return left(ServerFailure(e.toString()));
   }
 }
